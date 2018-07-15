e6581544f5b6eeb5758a681923f344e29980de50
hugang <hugang@meituan.com>
2018-07-15 14:27:17 +0800

    fix

---
+++ src/main/java/com/tudou/springboot/SpringBootTudouApplication.java
@@ -0,0 +1,12 @@
+package com.tudou.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTudouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTudouApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tudou/springboottudou/SpringBootTudouApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tudou.springboottudou;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootTudouApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
